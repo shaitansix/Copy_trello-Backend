99596e758e54b746ac4664870f880294d0f3e4eb
shaitansix <shaitansix3@gmail.com>
2025-01-23 19:31:34 -0500

    Configuracion del archivo vercel.json.

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,5 +1,13 @@
+// localhost
+// export const corsOptions = {
+//     origin: 'http://localhost:5173', 
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+//     allowedHeaders: ['Content-Type']
+// }
+
+// vercel
 export const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'https://copy-trello-frontend.vercel.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type']
 }
